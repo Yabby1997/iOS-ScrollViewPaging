443f9e23b0467c9b8645e97dfe420f6aaa2aeab6
yabby <yabby1997@gmail.com>
2022-04-18 17:26:58 +0900

    initial commit

---
+++ ScrollViewPaging/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollViewPaging
+//
+//  Created by Seunghun Yang on 2022/04/18.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
